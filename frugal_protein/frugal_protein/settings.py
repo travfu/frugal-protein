3b933a7d6ac8f56ba4dfd90227c45da44ed88bcc
Travis Fu <travis.fu@hotmail.com>
2019-10-05 20:26:33 +0100

    edit database config to work with local postgres

--- frugal_protein/frugal_protein/settings.py
+++ frugal_protein/frugal_protein/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from ._local_psql_credentials import postgres_username, postgres_password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,9 +77,12 @@ WSGI_APPLICATION = 'frugal_protein.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'frugal_protein_local',
+        'USER': postgres_username,
+        'PASSWORD': postgres_password,
+        'HOST': 'localhost',
+        'PORT': '5432',
 }
 
 
