f83241a93bccc8681548b4a9b19b94375000e837
Travis Fu <travis.fu@hotmail.com>
2019-10-05 20:41:08 +0100

    initialise class-based view for product page

--- frugal_protein/products/views.py
+++ frugal_protein/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from . import models as m
 
 # Create your views here.
+class ProductView(DetailView):
+    context_object_name = 'product'
+    model = m.ProductInfo
