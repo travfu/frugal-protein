36f1e3ca298e0a3abd4a1897e9681f0f12d965e0
Travis Fu <travis.fu@hotmail.com>
2019-10-08 22:54:36 +0100

    basic prototype for search page

--- frugal_protein/products/urls.py
+++ frugal_protein/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('search/', views.SearchView.as_view(), name='search'),
     path('product/<int:pk>/', views.ProductView.as_view(), name='product'),
 ]
--- frugal_protein/products/views.py
+++ frugal_protein/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from . import models as m
 
@@ -8,3 +8,9 @@ class ProductView(DetailView):
     context_object_name = 'product'
     model = m.ProductInfo
     template_name = 'products/product.html'
+
+class SearchView(ListView):
+    queryset = m.ProductInfo.objects.filter(description__icontains = 'quorn')
+    context_object_name = 'products'
+    template_name = 'products/product_search.html'
+    
