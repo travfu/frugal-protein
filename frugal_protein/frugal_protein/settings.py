960b0a7bf663e828cf6606a69cd97f72d733351c
Travis Fu <travis.fu@hotmail.com>
2019-10-08 22:17:07 +0100

    use django-heroku package to overwrite Django settings (e.g. DATABASE) to work with Heroku

--- frugal_protein/frugal_protein/settings.py
+++ frugal_protein/frugal_protein/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 from ._local_psql_credentials import postgres_username, postgres_password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
