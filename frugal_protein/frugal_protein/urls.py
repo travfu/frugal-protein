7226f0b080d428ed832b74c07e03071c529a60f6
Travis Fu <travis.fu@hotmail.com>
2019-11-05 16:30:37 +0000

    setup url dispatching and templating for calc page

--- frugal_protein/frugal_protein/urls.py
+++ frugal_protein/frugal_protein/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import calculator.views as calculator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls'))
+    path('', include('products.urls')),
+    path('protein-price-calculator', calculator.calculator)
 ]
