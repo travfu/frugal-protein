4c1f5ac504b2cd289eaa719faca549e1a92b2768
Travis Fu <travis.fu@hotmail.com>
2019-11-06 23:26:04 +0000

    add form to HTML response (via context)

--- frugal_protein/calculator/views.py
+++ frugal_protein/calculator/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from calculator.forms import ProteinCalcInput
 
 class ProteinCalculator(TemplateView):
     template_name = 'calculator/calc.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ProteinCalcInput
+        return context
