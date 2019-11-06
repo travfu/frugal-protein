3fd9d1bf75841857653734ce58a9071dfc736d6c
Travis Fu <travis.fu@hotmail.com>
2019-11-06 23:33:51 +0000

    refactor setUp to setUpClass, which is run once for whole class rather than every method

--- frugal_protein/calculator/tests.py
+++ frugal_protein/calculator/tests.py
@@ -2,9 +2,11 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 class TestCalculator(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.url = reverse('protein_calculator')
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.client = Client()
+        cls.url = reverse('protein_calculator')
 
     def test_url_dispatcher(self):
         response = self.client.get(self.url)
