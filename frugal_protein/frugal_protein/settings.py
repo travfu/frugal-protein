a60141ed390b9a96fbfaf2b7d5445c4621904afd
Travis Fu <travis.fu@hotmail.com>
2019-11-29 22:39:13 +0000

    new app + add to settings.py

--- frugal_protein/frugal_protein/settings.py
+++ frugal_protein/frugal_protein/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'calculator.apps.CalculatorConfig',
+    'scrape.apps.ScrapeConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
