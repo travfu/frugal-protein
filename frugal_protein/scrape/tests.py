be73b6e4e615e0eacaccf8643a12223bd74a1b9e
Travis Fu <travis.fu@hotmail.com>
2019-12-02 19:54:41 +0000

    added tests for input handling

--- frugal_protein/scrape/tests.py
+++ frugal_protein/scrape/tests.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.core.management import call_command
 from django.core.management.base import CommandError
 from django.test import TestCase
@@ -25,3 +27,21 @@ class ScrapeCommandTest(TestCase):
         """ Error should be raised if unimplemented store is passed via -s """
         with self.assertRaises(CommandError):
             call_command('scrape', 'info', '-s=randomstore')
+
+    def test_correct_handling_scrape_id(self):
+        """ Passing in 'id' arg should call scrape_ids method """
+        with patch.object(Command, 'scrape_ids') as mock_method:
+            call_command('scrape', 'id', '-a')
+            mock_method.assert_called_once()
+
+    def test_correct_handling_scrape_info(self):
+        """ Passing in 'info' arg should call scrape_infos method """
+        with patch.object(Command, 'scrape_infos') as mock_method:
+            call_command('scrape', 'info', '-a')
+            mock_method.assert_called_once()
+    
+    def test_correct_handling_scrape_price(self):
+        """ Passing in 'price' arg should call scrape_prices method """
+        with patch.object(Command, 'scrape_prices') as mock_method:
+            call_command('scrape', 'price', '-a')
+            mock_method.assert_called_once()
