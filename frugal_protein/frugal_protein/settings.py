90bd569f96d0aa2c6458dce22ad79385dfbe7768
Travis Fu <travis.fu@hotmail.com>
2019-12-15 20:58:18 +0000

    change order of INSTALLED_APPS - apps loaded top to bot, django apps should load first

--- frugal_protein/frugal_protein/settings.py
+++ frugal_protein/frugal_protein/settings.py
@@ -34,16 +34,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'products.apps.ProductsConfig',
-    'calculator.apps.CalculatorConfig',
-    'commands.apps.CommandsConfig',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'products.apps.ProductsConfig',
+    'calculator.apps.CalculatorConfig',
+    'commands.apps.CommandsConfig',
 
     'storages'
 ]
