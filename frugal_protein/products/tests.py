ffd3834eb0aa67af684ab06ecc365f7ba0200a2f
Travis Fu <travis.fu@hotmail.com>
2019-12-16 00:38:09 +0000

    added some tests for forms

--- frugal_protein/products/tests.py
+++ frugal_protein/products/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
+from .forms import ProductSearchForm
 from .helper.price_calc import Calc
+from .models import ProductInfo, Brands
 from .templatetags import products_filters as filters
 
 class TestCalculations(TestCase):
@@ -111,3 +113,81 @@ class TestFilters(TestCase):
     def test_formatuom_case_5(self):
         res = filters.formatuom('kg')
         self.assertEqual(res, 'kg')
+
+
+class TestForms(TestCase):
+    # forms.py
+
+    # Note that ProductSearchForm is designed so that a client makes a query for
+    # product(s) via a GET request and the query values are passed into as 
+    # initial args when instantiating the form. The form then generated a list
+    # of brands and stores associated with products returned by the search 
+    # query. This way, the query persists in the search bar but also, the 
+    # dropdown options for brand and store will dynamically change depening on 
+    # the search query.
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.brandA = Brands.objects.create(brand='brand A')
+        cls.brandB = Brands.objects.create(brand='brand B')
+        cls.brandC = Brands.objects.create(brand='brand C')
+
+    def test_brand_options_behaviour(self):
+        """ 
+        Brand choicefield should return a dynamic list of brands that is
+        dependent on the search query
+        """
+        # 3 products, 3 brands
+        ProductInfo.objects.create(description='turkey A', 
+                                   brand_id=self.brandA.brand_id)
+        ProductInfo.objects.create(description='turkey B',
+                                   brand_id=self.brandB.brand_id)
+        ProductInfo.objects.create(description='chicken',
+                                   brand_id=self.brandC.brand_id)
+        
+        # Mock search query
+        mock_query = {'initial': {'search': 'turkey'}}
+        bound_form = ProductSearchForm(**mock_query)
+        brand_choices = bound_form.fields['brand'].choices
+
+        # Assert brands associated with search query is found in dropdown 
+        # options (ie. brand A & B, but not C)
+        e_1 = (self.brandA.brand_id, self.brandA.brand.title())
+        e_2 = (self.brandB.brand_id, self.brandB.brand.title())
+        e_3 = (self.brandC.brand_id, self.brandC.brand.title())
+        self.assertIn(e_1, brand_choices)
+        self.assertIn(e_2, brand_choices)
+        self.assertNotIn(e_3, brand_choices)
+
+    def test_brand_options_uses_full_txt_search(self):
+        """         
+        Brands listed in dropdown should be should match the products returned
+        by get_queryset in Views, which uses psql full text search. Thus,
+        the brand field should also utilise full text search
+        """
+        # Standard PSQL query for 'turkey mince' will not match 'turkey xxx 
+        # mince', but full text search will.
+        ProductInfo.objects.create(description='turkey xxx mince', 
+                                   brand_id=self.brandA.brand_id)
+        mock_query = {'initial': {'search': 'turkey mince'}}
+        bound_form = ProductSearchForm(**mock_query)
+        brand_choices = bound_form.fields['brand'].choices
+        
+        e_1 = (self.brandA.brand_id, self.brandA.brand.title())
+        e_2 = (self.brandB.brand_id, self.brandB.brand.title())
+        self.assertIn(e_1, brand_choices)
+        self.assertNotIn(e_2, brand_choices)
+
+    def test_store_options_behaviour(self):
+        """ 
+        Store choicefield should return a list of all stores, independently of
+        product query
+        """
+        ProductInfo.objects.create(description='turkey', tesco='123',
+                                   iceland=None)
+        mock_query = {'initial': {'search': 'turkey'}}
+        bound_form = ProductSearchForm(**mock_query)
+        store_choices = bound_form.fields['store'].choices
+
+        self.assertIn(('tesco', 'Tesco'), store_choices)
+        self.assertIn(('iceland', 'Iceland'), store_choices)
