1567e4c631d90cd3ce76b16c40d9af5f1d0003a6
Travis Fu <travis.fu@hotmail.com>
2019-12-16 00:37:54 +0000

    clean

--- frugal_protein/products/forms.py
+++ frugal_protein/products/forms.py
@@ -3,10 +3,11 @@ from .models import Brands, ProductInfo
 
 class myWidget(forms.Select):
     """
-    forms.Select generates an html string for <select> froms with <options> and applies 
-    user-specified attributes across all <options>. 
+    forms.Select generates an html string for <select> froms with <options> and 
+    applies user-specified attributes across all <options>. 
 
-    This custom widget add the 'disabled' attribute to <options> with a specific value.
+    This custom widget add the 'disabled' attribute to <options> with a specific 
+    value.
     doc: https://github.com/django/django/blob/master/django/forms/widgets.py
     """
     def get_context(self, *args, **kwargs):
@@ -14,7 +15,6 @@ class myWidget(forms.Select):
         for _, option, _ in context['widget']['optgroups']:
             value = option[0].get('value')
             if value == '':
-                # Add disabled attr to attrs dict (this is possible because of mutability)
                 option[0]['attrs']['disabled'] = ''
         return context
 
@@ -47,10 +47,13 @@ class ProductSearchForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Goal: 
-        #   1) populate brand dropdown with brand name of products returned by search query
-        #   2) populate store dropdown with a list of all the stores (independent of search query) 
+        #   1) populate brand dropdown with brand name of products returned by 
+        #      search query
+        #   2) populate store dropdown with a list of all the stores 
+        #      (independent of search query) 
 
-        # The search query can be found in the 'initial' kwarg when the form is instantiated
+        # The search query can be found in the 'initial' kwarg when the form is 
+        # instantiated
         search_query = kwargs['initial'].get('search')
         if search_query is not None:
             self.fields['brand'].choices = self.get_brand_choices(search_query)
@@ -65,7 +68,7 @@ class ProductSearchForm(forms.Form):
             productinfo__description__search=search_query)
         brands = brands.distinct().order_by('brand')
 
-        choice_tuples = [(brand.brand_id, brand.brand.title()) for brand in brands]
+        choice_tuples = [(b.brand_id, b.brand.title()) for brand in brands]
         if choice_tuples:
             brand_count_str = str(len(choice_tuples))
             # Assign value '0' for 'all brands'
